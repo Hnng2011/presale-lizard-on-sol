1fcd06af341500a125e9c9ad477e13ba2fb2b95a
Hnng2011 <luphihung111@gmail.com>
2024-03-13 22:44:05 +0700

    update

--- components/SendSolForm.tsx
+++ components/SendSolForm.tsx
@@ -179,7 +179,7 @@ const SendSolForm = () => {
 
                 const wlData = await wl.json();
                 setKOL(Boolean(wlData.isKOL));
-                setCurBuy(wlData?.currentbuy.toString());
+                setCurBuy(wlData?.currentbuy?.toString());
                 setWL(true);
                 console.log('WL:', wlData);
             } catch (error) {
