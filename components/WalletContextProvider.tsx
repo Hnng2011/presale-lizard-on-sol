2fe538f1580d9ba22cf36bf2afae94ecb5f1c2a8
Hnng2011 <luphihung111@gmail.com>
2024-03-13 22:21:30 +0700

    update

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { WalletAdapterNetwork, WalletAdapter } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import * as web3 from '@solana/web3.js'
 require('@solana/wallet-adapter-react-ui/styles.css');
 
@@ -13,6 +13,7 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const wallets = useMemo(
         () => [
             new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
         ],
         [network]
     );
